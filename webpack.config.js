502f94df227b108b7f53862f96ac21a3941afbe5
Nastia Kartseva <danetzachem@gmail.com>
2022-06-01 19:29:17 +0300

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
 const path = require('path');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const HtmlWebPackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+// const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 const env = process.env.NODE_ENV || 'development';
 const isProd = env === 'production';
 
 module.exports = {
   mode: env,
-  entry: [
-    './app/assets/stylesheets/application.scss',
-    './app/javascript/application.js',
-  ],
+  entry: './app/javascript/application.js',
   output: {
-    publicPath: '/assets/',
+    publicPath: '/',
     path: path.resolve(__dirname, 'public/assets'),
   },
   devtool: isProd ? false : 'eval-source-map',
@@ -25,10 +24,10 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   externals: {
-    gon: 'gon',
     net: 'net',
     fs: 'fs',
     tls: 'tls',
+    os: 'os',
     child_process: 'child_process',
     'node-pty': 'node-pty',
     express: 'express',
@@ -46,16 +45,10 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
           { loader: MiniCssExtractPlugin.loader },
           { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' },
         ],
       },
       {
@@ -68,43 +61,28 @@ module.exports = {
           loader: 'file-loader',
         },
       },
+      {
+        test: /\.css$/i,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+
+      {
+        test: /\.(jpg|png|jpeg)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
     ],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
   },
   plugins: [
-    // new CleanWebpackPlugin(),
-    // new HtmlWebPackPlugin({
-    //   template: './public/index.html',
-    //   filename: './index.html',
-    // }),
-    new MiniCssExtractPlugin({
-      // filename: '[name].[contenthash].css',
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
     }),
-    // new CopyPlugin([
-    //   { from: './public/favicon.ico', to: './favicon.ico' },
-    //   { from: './public/manifest.json', to: './manifest.json' },
-    //   { from: './public/logo192.png', to: './logo192.png' },
-    // ]),
-    new MonacoWebpackPlugin({
-      languages: ['javascript', 'php', 'python', 'typescript'],
-    }),
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
-  optimization: {
-    minimizer: [
-      new TerserPlugin(),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
-    // Automatically split vendor and commons
-    // https://twitter.com/wSokra/status/969633336732905474
-    // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-    // splitChunks: {
-    //   chunks: 'all',
-    //   name: false,
-    // },
-    // Keep the runtime chunk separated to enable long term caching
-    // https://twitter.com/wSokra/status/969679223278505985
-    // https://github.com/facebook/create-react-app/issues/5358
-    // runtimeChunk: {
-    //   name: (entrypoint) => `runtime-${entrypoint.name}`,
-    // },
-  },
 };
