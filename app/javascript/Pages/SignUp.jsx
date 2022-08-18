ead2264e93f7d72ca904dcc67392eec9f94d8cc9
Sverxsro4nik <Sverxsro4nik@gmail.com>
2022-08-18 16:39:39 +0500

    [#64 update validation login in signUpValidation]

--- app/javascript/Pages/SignUp.jsx
+++ app/javascript/Pages/SignUp.jsx
@@ -4,9 +4,20 @@ import { useFormik } from 'formik';
 
 export function SignUp() {
   const signUpValidation = yup.object().shape({
-    name: yup.string().mix().max().trim().typeError().required(),
-    email: yup.string().email(),
-    password: yup.string().trim().min().typeError().required(),
+    name: yup
+      .string()
+      .matches(/^[A-Za-z ]*$/, 'Please enter valid name')
+      .trim()
+      .typeError()
+      .required(),
+    email: yup.string().email('Incorrect email'),
+    password: yup
+      .string()
+      .trim()
+      .min(8)
+      .max(30)
+      .typeError('The password must be 8 to 30 characters long')
+      .required(),
     confirmPassword: yup
       .string()
       .test(
