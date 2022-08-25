853105d2e721545fb8542c7a06785b0d1ea326cc
Sverxsro4nik <Sverxsro4nik@gmail.com>
2022-08-25 21:24:28 +0500

    delete eslint rules

--- app/javascript/App.jsx
+++ app/javascript/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/function-component-definition */
 import React from 'react';
 import { Editor } from './components/Editor/index.jsx';
 import { Button } from './components/Button/index.jsx';
