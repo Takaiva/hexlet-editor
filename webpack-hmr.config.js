629090a2e4e90c5f6a309ce618ada02890d06a07
Тимур Хабибов <Tim-him@mail.ru>
2022-08-30 20:24:28 +0500

    snippets

--- webpack-hmr.config.js
+++ webpack-hmr.config.js
@@ -21,7 +21,7 @@ module.exports = function (options, webpack) {
       }),
       new RunScriptWebpackPlugin({
         name: options.output.filename,
-        autoRestart: false,
+        autoRestart: true,
       }),
     ],
   };
