aaea536774795f05168b186888f9c26ecfce67b6
Тимур Хабибов <Tim-him@mail.ru>
2022-09-08 09:45:05 +0500

    deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,7 +3,7 @@ import { snippets1662489659089 } from './migrations/1662489659089-snippets';
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DATABASE_URL,
   port: 5432,
   username: 'timyr',
   password: '1234',
