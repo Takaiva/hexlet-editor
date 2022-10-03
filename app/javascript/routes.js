39fa66b4ac4d09ffd5d7673f797221bb23ebcb64
Александр Сдобников <sdobnikove@mail.ru>
2022-10-03 03:26:29 +0300

    add separte file with all routes to easily manage routes on-the-go #119

---
+++ app/javascript/routes.js
@@ -0,0 +1,9 @@
+export default () => ({
+  homePagePath: '/',
+  aboutPagePath: '/about',
+  profilePagePath: '/profile',
+  loginPagePath: '/login',
+  signUpPagePath: '/signup',
+  remindPassPagePath: '/remind_password',
+  replsPagePath: '/repls',
+});
