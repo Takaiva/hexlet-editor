693796c94bdcd87a8a76440ff79455969753f9b1
Александр Сдобников <sdobnikove@mail.ru>
2022-10-10 10:14:35 +0300

    add snippet fetching, add snippets to user profile

--- app/javascript/Pages/Profile.jsx
+++ app/javascript/Pages/Profile.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import { actions } from '../slices';
+
+import axios from 'axios';
+import routes from '../routes.js';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export function Profile() {
+  const [snippets, setSnippets] = useState([]);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  //const [userdata, setUserdata] = useState([]);
+
+  const openTerminal = (code) => () => {
+    dispatch(actions.updateCode(code)); // далее роутинг на App
+    navigate(routes.homePagePath());
+  };
+
+  useEffect(() => {
+    const fetchUserSnippets = async() => {
+      const response = await axios.get(routes.userProfilePath());
+      setSnippets(response.data);
+      // setUserdata(response.data.user); set user data
+    }
+    fetchUserSnippets();
+  }, []);
+
   return (
     <Container className="h-100 my-4 overflow-hidden rounded shadow">
       <Row className="h-100 bg-white flex-md-row">
@@ -10,14 +35,18 @@ export function Profile() {
             <img
               className="rounded-circle"
               src="#" /* Добавить ссылку */
-              alt="User avatar"
+              alt="User avatar" /* добавить дефолтную картинку пользователя */
             />
           </div>
           <h2 className="my-2">
-            Username {/* Добавить определение текущего пользователя */}
+            Username {/* user.username */}
+            CreatedAt {/* user.created_at */}
           </h2>
           <h3 className="my-2">
-            UserId {/* Добавить определение текущего пользователя */}
+            UserId {/* user.userId */}
+          </h3>
+          <h3 className="my-2">
+            UserEmail {/* user.email */}
           </h3>
           <div>
             <Button>Edit {/* Добавить возможность редактирования */}</Button>
@@ -29,6 +58,21 @@ export function Profile() {
         <Col xs={12} md={9} my={4} className="p-0 h-100">
           <div className="d-flex flex-column h-100">
             <h2>Repls</h2>
+            <Row xs={1} md={2} className="g-4">
+            {snippets.map(({ id, name, code }) => (
+              <Col xs lg="3" key={id}>
+                <Card border="primary">
+                  <Card.Header>{name}</Card.Header>
+                  <Card.Body>
+                    <Card.Text>Изображения нет</Card.Text>
+                    <Button variant="primary" onClick={openTerminal(code)}>
+                      Open repl
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+            </Row>
           </div>
         </Col>
       </Row>
