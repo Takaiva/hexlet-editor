27e02fdb58bc06d1c45ea7ae20bec468604fcbab
Александр Сдобников <sdobnikove@mail.ru>
2022-10-10 10:07:06 +0300

    update validation, update texts, update layout as signup component got rendered 10 times when mounted

--- app/javascript/Pages/SignUp.jsx
+++ app/javascript/Pages/SignUp.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/function-component-definition */
 /* eslint-disable no-console */
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
 import {
@@ -10,15 +11,21 @@ import {
   Col,
   Container,
   Form,
-  FormControl,
-  FormLabel,
   Row,
 } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useAuth } from '../hooks/'
+
+import axios from 'axios';
+
+import routes from '../routes.js';
 
 export const SignUp = () => {
   const inputRef = useRef();
   const { t } = useTranslation();
+  const [regFailed, setRegFailed] = useState(false);
+  const auth = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
     inputRef.current.focus();
@@ -27,22 +34,25 @@ export const SignUp = () => {
   const signUpValidation = yup.object().shape({
     name: yup
       .string()
-      .matches(/^[A-Za-z ]*$/, t('signUp.nameValidation'))
       .trim()
+      .min(3, t('signUp.validation.usernameLength'))
+      .max(20, t('signUp.validation.usernameLength'))
+      .matches(/^[A-Za-z ]*$/, t('signUp.validation.correctUsername'))
       .typeError()
-      .required(),
-    email: yup.string().email(t('signUp.emailValidation')),
+      .required(t('signUp.validation.requiredField')),
+    email: yup.string().email(t('signUp.validation.correctEmail')).required(t('signUp.validation.requiredField')),
     password: yup
       .string()
       .trim()
-      .min(8)
-      .max(30)
-      .typeError(t('signUp.passwordLenght'))
-      .required(),
+      .min(8, t('signUp.validation.passwordLength'))
+      .max(30, t('signUp.validation.passwordLength'))
+      .typeError()
+      .required(t('signUp.validation.requiredField')),
     confirmPassword: yup
       .string()
       .test(
         'confirmPassword',
+        t('signUp.validation.confirmPassword'),
         (password, context) => password === context.parent.password,
       ),
   });
@@ -54,90 +64,139 @@ export const SignUp = () => {
       confirmPassword: '',
     },
     validationSchema: signUpValidation,
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: (values, actions) => {
+      actions.setSubmitting(true);
+      axios.post(routes.usersPath(),
+        {
+          name: values.name,
+          email: values.email,
+          password: values.password,
+          confirmPassword: values.confirmPassword,
+        })
+        .then((response) => {
+          console.log(response.data);
+          auth.logIn(response.data);
+          navigate(routes.homePagePath());
+          actions.setSubmitting(false);
+        })
+        .catch((error) => {
+          if (!error.isAxiosError) {
+            console.log(t('errors.unknown'));
+            throw error;
+          }
+          if (error.response.status === 409) {
+            setRegFailed(true);
+            inputRef.current.select();
+          } else {
+            console.log(t('errors.network'));
+            throw error;
+          }
+          actions.setSubmitting(false);
+        });
     },
   });
-  const { handleBlur, handleChange, handleSubmit, values } = formik;
   return (
-    <Container fluid className="h-100">
+    <Container className="h-100">
       <Row className="justify-content-center align-content-center h-100">
-        <Col xs={12} md={8} xxl={5} className="mt-5">
-          <div className="pb-lg-5">
-            <Card className="shadow-sm">
-              <Card.Body className="p-lg-4 p-xl-5">
-                <h1 className="mb-4 fw-light">{t('signUp.pageHeader')}</h1>
-                <div className="pt-lg-3">
-                  <Form onSubmit={handleSubmit}>
-                    <div className="mb-2 email required">
-                      <FormLabel htmlFor="email">
-                        {t('signUp.email')} <span>*</span>
-                      </FormLabel>
-                      <FormControl
-                        ref={inputRef}
-                        type="text"
-                        autoFocus="autofocus"
-                        name="email"
-                        id="email"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.email}
-                      />
-                    </div>
-                    <div className="mb-2">
-                      <FormLabel htmlFor="name">
-                        {t('signUp.username')}
-                      </FormLabel>
-                      <FormControl
-                        type="text"
-                        autoFocus="autofocus"
-                        name="name"
-                        id="name"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.name}
-                      />
-                    </div>
-                    <div className="mb-2">
-                      <FormLabel htmlFor="password">
-                        {t('signUp.userPassword')}
-                      </FormLabel>
-                      <FormControl
+        <Col xs={12} md={6} xxl={5} className="mt-5">
+          <Card className="shadow-sm">
+            <Card.Body className="p-lg-4 p-xl-5">
+              <h1 className="mb-4 fw-light">{t('signUp.pageHeader')}</h1>
+              <div className="pt-lg-3">
+                <Form onSubmit={formik.handleSubmit} noValidate>
+                  <Form.Group className="mb-2">
+                    <Form.Label htmlFor="email">{t('signUp.emailLabel')}</Form.Label>
+                    <Form.Control
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                      onBlur={formik.handleBlur}
+                      className="form-input"
+                      name="email"
+                      isInvalid={(formik.touched['email'] && formik.errors['email']) || regFailed}
+                      id="email"
+                      autoComplete="email"
+                      required
+                      ref={inputRef}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {(formik.touched['email'] && formik.errors['email']) || regFailed}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+
+                  <Form.Group className="mb-2">
+                    <Form.Label htmlFor="name">{t('signUp.usernameLabel')}</Form.Label>
+                    <Form.Control
+                      onChange={formik.handleChange}
+                      value={formik.values.name}
+                      onBlur={formik.handleBlur}
+                      className="form-input"
+                      name="name"
+                      id="name"
+                      autoComplete="username"
+                      required
+                      isInvalid={(formik.touched['name'] && formik.errors['name']) || regFailed}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                       {formik.errors['name'] ? formik.errors['name'] : regFailed}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+
+                    <Form.Group className="mb-2">
+                      <Form.Label htmlFor="password">{t('signUp.passwordLabel')}</Form.Label>
+                      <Form.Control
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        onBlur={formik.handleBlur}
                         type="password"
-                        autoFocus="autofocus"
+                        className="form-input"
                         name="password"
                         id="password"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.password}
+                        autoComplete="new-password"
+                        required
+                        isInvalid={(formik.touched['password'] && formik.errors['password']) || regFailed}
                       />
-                    </div>
-                    <div className="mb-2">
-                      <FormLabel htmlFor="confirmPassword">
-                        {t('signUp.confirmUserPassword')}
-                      </FormLabel>
-                      <FormControl
-                        type="password"
-                        autoFocus="autofocus"
-                        name="confirmPassword"
-                        id="confirmPassword"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.confirmPassword}
-                      />
-                    </div>
-                    <Button
-                      type="submit"
-                      variant="primary"
-                      className="w-100 mt-4"
-                    >
-                      {t('signUp.register')}
-                    </Button>
-                  </Form>
+                      <Form.Control.Feedback type="invalid">
+                        {formik.errors['password'] ? formik.errors['password'] : regFailed}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                  <Form.Group className="mb-2">
+                    <Form.Label htmlFor="confirmPassword">{t('signUp.confirmPasswordLabel')}</Form.Label>
+                    <Form.Control
+                      onChange={formik.handleChange}
+                      value={formik.values.confirmPassword}
+                      onBlur={formik.handleBlur}
+                      type="password"
+                      className="form-input"
+                      name="confirmPassword"
+                      id="confirmPassword"
+                      autoComplete="new-password"
+                      required
+                      isInvalid={(formik.touched['confirmPassword'] && formik.errors['confirmPassword']) || regFailed}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formik.errors['confirmPassword'] ? formik.errors['confirmPassword'] : regFailed}
+                      </Form.Control.Feedback>
+                  </Form.Group>
+                  <Button
+                    type="submit"
+                    variant="primary"
+                    className="w-100 mt-3 pt-2 pb-2"
+                    data-disable-with="Войти"
+                  >
+                    {t('signUp.registerButton')}
+                  </Button>
+                </Form>
+              </div>
+            </Card.Body>
+            <Card.Footer className="border-top-0 text-center py-3">
+              <div className="py-lg-2">
+                <div>
+                  <span className="text-muted">{t('signUp.footer.signInHeader')}</span>
+                  <a className="link-dark" href={routes.loginPagePath()}>{t('signUp.footer.signIn')}</a>
                 </div>
-              </Card.Body>
-            </Card>
-          </div>
+              </div>
+            </Card.Footer>
+          </Card>
         </Col>
       </Row>
     </Container>
