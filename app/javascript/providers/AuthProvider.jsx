6b6c0acab2b90176c6afe3e4cf8772fcfdc0cefe
Александр Сдобников <sdobnikove@mail.ru>
2022-10-10 09:56:53 +0300

    add custom context/hooks, add auth provider

---
+++ app/javascript/providers/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from '../contexts';
+import axios from 'axios';
+import routes from '../routes.js';
+
+function AuthProvider({ children }) {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const logOut = async () => {
+      await axios.post(routes.logoutPath());
+      setLoggedIn(null);
+  };
+
+  useEffect(() => {
+   /* const fetchAuthData = async () => {
+      const response = await axios.get('api/checkAuth'); // boolean: проверка на авторизированность
+      setLoggedIn(response.data);
+    }*/
+    // fetchAuthData();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        logOut,
+        isLoggedIn,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
